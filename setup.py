3569dc26f3ff3b9a73dcbaa9d7c12f13827bbe91
yousefalshaikh17 <164582835+yousefalshaikh17@users.noreply.github.com>
2025-03-23 04:12:22 +0000

    Removed unnecessary comments

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-# from mc_server_manager import __version__
 
 def read_version():
     with open("mc_server_manager/version.txt", "r") as f:
@@ -8,7 +7,6 @@ def read_version():
 setup(
     name="mc-server-manager",
     version=read_version(),
-    # packages=find_packages(),
     install_requires=[
         "process-controller @ git+https://github.com/yousefalshaikh17/system-process-controller.git",
         "python-dotenv",
